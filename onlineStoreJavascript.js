0c74a547b903c7ff4c3ecd97d7c956da02813776
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-09 14:20:41 +0100

    In JS obtained references to the elements that need changing

---
+++ onlineStoreJavascript.js
@@ -0,0 +1,31 @@
+//Ui elements
+let imageOne;
+let imageTwo;
+let imageThree;
+let imageFour;
+let imageFive;
+let imageSix;
+let itemTextOne;
+let itemTextTwo;
+let itemTextThree;
+let itemTextFour;
+let itemTextFive;
+let itemTextSix;
+
+function init() {
+	imageOne = document.querySelector('#storeItemImageOne');
+	imageOne = document.querySelector('#storeItemImageTwo');
+	imageOne = document.querySelector('#storeItemImageThree');
+	imageOne = document.querySelector('#storeItemImageFour');
+	imageOne = document.querySelector('#storeItemImageFive');
+	imageOne = document.querySelector('#storeItemImageSix');
+	itemTextOne = document.querySelector('#storeItemTextOne');
+	itemTextTwo = document.querySelector('#storeItemTextTwo');
+	itemTextThree = document.querySelector('#storeItemTextThree');
+	itemTextFour = document.querySelector('#storeItemTextFour');
+	itemTextFive = document.querySelector('#storeItemTextFive');
+	itemTextSix = document.querySelector('#storeItemTextSix');
+}
+
+
+window.onload = init;
