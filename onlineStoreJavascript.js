9ff6f36f9c706d58f4258f4413090684402ecba7
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-09 14:33:26 +0100

    Added method for when second button clicked

--- onlineStoreJavascript.js
+++ onlineStoreJavascript.js
@@ -11,6 +11,8 @@ let itemTextThree;
 let itemTextFour;
 let itemTextFive;
 let itemTextSix;
+let buttonOne;
+let buttonTwo;
 
 function populateWhenOneClicked() {
 	imageOne.src = "Images/Items/BoatModel.PNG";
@@ -20,12 +22,28 @@ function populateWhenOneClicked() {
 	imageFive.src = "Images/Items/NormanModel.PNG";
 	imageSix.src = "Images/Items/PyramidModel.PNG";
 
-	itemTextOne.textContent = "Boat Model £79.99"
-	itemTextTwo.textContent = "German Soldier Model £11.99"
-	itemTextThree.textContent = "German Soldier Model £11.99"
-	itemTextFour.textContent = "Motte Castle Model £49.99"
-	itemTextFive.textContent = "Norman Model £8.99"
-	itemTextSix.textContent = "Pyramid Model £14.99"
+	itemTextOne.textContent = "Boat Model £79.99";
+	itemTextTwo.textContent = "German Soldier Model £11.99";
+	itemTextThree.textContent = "German Soldier Model £11.99";
+	itemTextFour.textContent = "Motte Castle Model £49.99";
+	itemTextFive.textContent = "Norman Model £8.99";
+	itemTextSix.textContent = "Pyramid Model £14.99";
+}
+
+function populateWhenTwoClicked() {
+	imageOne.src = "Images/Items/RomeCastleModel.PNG";
+	imageTwo.src = "Images/Items/SasModel.PNG";
+	imageThree.src = "Images/Items/SaxonModel.PNG";
+	imageFour.src = "Images/Items/SwatModel.PNG";
+	imageFive.src = "Images/Items/VikingModel.PNG";
+	imageSix.src = "Images/Items/WW1Model.PNG";
+
+	itemTextOne.textContent = "Roman Castle Model £199.99";
+	itemTextTwo.textContent = "SaS Model £6.99";
+	itemTextThree.textContent = "Saxon Model £8.99";
+	itemTextFour.textContent = "Swat Model £9.99";
+	itemTextFive.textContent = "Viking Model £8.99";
+	itemTextSix.textContent = "WW1 Model £13.99";
 }
 
 function init() {
@@ -41,6 +59,8 @@ function init() {
 	itemTextFour = document.querySelector('#storeItemTextFour');
 	itemTextFive = document.querySelector('#storeItemTextFive');
 	itemTextSix = document.querySelector('#storeItemTextSix');
+	buttonOne = document.querySelector('#storeItemButtonOne');
+	buttonTwo = document.querySelector('#storeItemButtonTwo');
 	populateWhenOneClicked();
 }
 
